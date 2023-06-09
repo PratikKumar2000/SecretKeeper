c1a6f0f043c601408ee89dcc5ca6944ca0562788
Pratik Kumar <kumarpratik2211@gmail.com>
2023-06-10 01:21:28 +0530

    made changes in url

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: `http://localhost:${port}/auth/google/secrets`,
+      callbackURL: `http://secret-keeper/auth/google/secrets`,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
